1012f40520f15162fc742aa5a76bea24595c225b
charmingprince90x <charmingprince90x@gmail.com>
2026-06-02 22:47:49 -0700

    Fix bootstrap/cache auto-create and permissions on startup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,18 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$basePath = dirname(__DIR__);
+$cachePath = $basePath.'/bootstrap/cache';
+
+if (! is_dir($cachePath)) {
+    mkdir($cachePath, 0775, true);
+}
+
+if (is_dir($cachePath) && ! is_writable($cachePath)) {
+    @chmod($cachePath, 0775);
+}
+
+return Application::configure(basePath: $basePath)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
